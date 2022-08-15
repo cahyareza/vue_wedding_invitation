42ad0be98ef599e161e85733747fbb2387d6940f
cahyafaisalreza <cf.reza07@gmail.com>
2022-08-15 13:11:10 +0700

    Bagian 2
    Meload data dari api, and rendered to template.

--- src/components/GroomBride.vue
+++ src/components/GroomBride.vue
@@ -8,13 +8,14 @@
                     <div class="column is-full">
                         <figure class="image is-inline-block" style="height:180px; width: 180px;">
                             <img class="is-rounded mb-3" src="https://bulma.io/images/placeholders/128x128.png">
-                            <a href="######"><font-awesome-icon icon="fa-brands fa-4x fa-instagram" /></a>
+                            <a :href="couples.perempuan?.instagram_link"><font-awesome-icon icon="fa-brands fa-4x fa-instagram" /></a>
                         </figure>
 
-                        <p class="title mt-5 mb-6">Nadia</p>
+                        <!-- <p class="title mt-5 mb-6" v-for="couple in couples.perempuan" :key="couple.id">{{ couple }}</p> -->
+                        <p class="title mt-5 mb-6" v-if="couples.perempuan">{{ couples.perempuan.name }}</p>
 
-                        <p class="subtitle is-size-6 has-text-weight-bold mb-1">Putri pertama dari</p>
-                        <p class="subtitle is-size-6 mt-1">Bapak Michael Stam & Ibu Aminah SA</p>
+                        <p class="subtitle is-size-6 has-text-weight-bold mb-1">Putri ke{{ couples.perempuan?.anak_ke }} dari</p>
+                        <p class="subtitle is-size-6 mt-1">Bapak {{ couples.perempuan?.nama_ayah }} & Ibu {{ couples.perempuan?.nama_ibu }}</p>
 
                         <p class="subtitle is-size-5 has-text-weight-bold mt-3">&</p>
 
@@ -23,13 +24,13 @@
                     <div class="column is-full mt-4">
                         <figure class="image is-inline-block" style="height:180px; width: 180px;">
                             <img class="is-rounded mb-3" src="https://bulma.io/images/placeholders/128x128.png">
-                            <a href="######"><font-awesome-icon icon="fa-brands fa-4x fa-instagram" /></a>
+                            <a :href="couples.laki_laki?.instagram_link"><font-awesome-icon icon="fa-brands fa-4x fa-instagram" /></a>
                         </figure>
 
-                        <p class="title mt-5 mb-6">Yudha</p>
+                        <p class="title mt-5 mb-6">{{ couples.laki_laki?.name }}</p>
 
-                        <p class="subtitle is-size-6 has-text-weight-bold mb-1">Putra pertama dari</p>
-                        <p class="subtitle is-size-6 mt-1">Bapak Bambang & Ibu Siti Susana</p>
+                        <p class="subtitle is-size-6 has-text-weight-bold mb-1">Putra ke{{couples.laki_laki?.anak_ke }} dari</p>
+                        <p class="subtitle is-size-6 mt-1">Bapak {{ couples.laki_laki?.nama_ayah }} & Ibu {{ couples.laki_laki?.nama_ibu }}</p>
                     </div>
                 </div>
 
@@ -41,19 +42,18 @@
     </section>
 </template>
 
+
 <script setup>
-import axios from 'axios'
 import { ref, onMounted } from 'vue'
 
-const couples = ref(null)
+const couples = ref([]);
 
 onMounted(() => {
-    axios.get('contents/couples.json')
-        .then( 
-            res => couples.value = res.data.couples,
-            console.log(couples.value)
-        )
-        .catch( err => alert(err) )
-})
-</script>
+    fetch(' http://localhost:3000/couples')
+    .then(response => response.json())
+    .then(data => {
+        couples.value = data
+    });
+});
 
+</script>
--- src/components/MainCover.vue
+++ src/components/MainCover.vue
@@ -3,7 +3,7 @@
         <section class="section">
             <p class="subtitle">Undangan Pernikahan</p>
         
-            <p class="title mt-5">Rossa $ Riyan</p>
+            <p class="title mt-5">{{couples.perempuan?.name }} & {{couples.laki_laki?.name }}</p>
 
             <p class="subtitle mt-4 is-size-6">17 . 7 . 2025</p>
         </section>
@@ -17,5 +17,16 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
+
+const couples = ref([]);
+
+onMounted(() => {
+    fetch(' http://localhost:3000/couples')
+    .then(response => response.json())
+    .then(data => {
+        couples.value = data
+    });
+});
 
 </script>
